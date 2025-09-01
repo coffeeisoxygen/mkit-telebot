433d27861f5b0453092de54e599e35f0f65189d3
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-09-01 19:50:50 +0700

    feat: implement logging decorators and configuration

---
+++ app/mlogging/__init__.py
@@ -0,0 +1,9 @@
+from app.custom.mlogging.decorators import log_entry_exit, log_exec_time, timeit
+from app.custom.mlogging.setup import setup_logging
+
+__all__ = [
+    "log_entry_exit",
+    "log_exec_time",
+    "timeit",
+    "setup_logging",
+]
