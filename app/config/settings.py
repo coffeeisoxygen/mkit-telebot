2c3a0333590c641af69e31d86b6f0f71606f447d
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-09-02 12:33:18 +0700

    feat: add JWT configuration to settings

--- app/config/settings.py
+++ app/config/settings.py
@@ -5,7 +5,12 @@ from pathlib import Path
 from loguru import logger
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from app.config.values import ConfigAdminAccount, ConfigDatabase, ConfigEnvironment
+from app.config.values import (
+    ConfigAdminAccount,
+    ConfigDatabase,
+    ConfigEnvironment,
+    ConfigJwt,
+)
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 DEFAULT_ENV_FILE = BASE_DIR / ".env"
@@ -22,6 +27,7 @@ class Settings(BaseSettings):
     ENV: ConfigEnvironment = ConfigEnvironment()
     DB: ConfigDatabase = ConfigDatabase()
     ADM: ConfigAdminAccount = ConfigAdminAccount()
+    JWT: ConfigJwt = ConfigJwt()
 
 
 @lru_cache
