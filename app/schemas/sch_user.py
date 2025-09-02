4faddad59ed9ef52772d32640ea79f7c9a35c6f1
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-09-02 11:01:54 +0700

    refactor: remove unused UserAdmin classes

--- app/schemas/sch_user.py
+++ app/schemas/sch_user.py
@@ -34,12 +34,3 @@ class UserPublicResponse(UserBase):
 
 class UserListResponse(BaseModel):
     users: list[UserPublicResponse]
-
-
-class UserAdminSeed(UserCreate):
-    is_superuser: bool = True
-    is_active: bool = True
-
-
-class UserAdminResponse(UserInDB):
-    pass
